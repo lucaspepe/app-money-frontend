330d01803f8a56522e31290c55d5e0f0f48db800
Lucas Carvalho <lukas_pe_pe@hotmail.com>
2022-01-19 20:50:57 -0300

    inicio do dashboard

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2022
+            <a href="http://pelas.com.br" target='_blank'></a>.
+        </strong>
+    </footer>
+)
