d2498b13548da8f32dfcbb8759e72292e787ed8d
Lucas Carvalho <lukas_pe_pe@hotmail.com>
2022-02-28 14:18:54 -0300

    aplicação antes do auth

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -4,14 +4,17 @@ import React from "react";
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
 import Footer from '../common/template/footer'
+import Routes from './routes'
+import Messages from '../common/msg/messages'
 
 export default props => (
     <div className="wrapper">
         <Header />
         <SideBar />
         <div className='content-wrapper'>
-            <h1>Conteúdo</h1>
+            <Routes />
         </div>
         <Footer />
+        <Messages />
     </div>
 )
